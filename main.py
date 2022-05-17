53c7f8411873cc222b84bab13b1165d1374c59cd
naut <jrbarnachea@gmail.com>
2022-05-16 18:05:32 -0700

    Update main.py

--- main.py
+++ main.py
@@ -40,30 +40,52 @@ def save_pdf_book_to_json(file=FILE_NAME):
     os.remove(file + ".pdf")
 
 
-def speak_pdf(text):
+def txt_to_speech(text):
     """Reads outloud inputted text."""
     # initialize the object
     pyttsx3_file = pyttsx3.init()
-
+    pyttsx3_file.setProperty('voice', "com.apple.speech.synthesis.voice.daniel")    
     pyttsx3_file.say(text)
     pyttsx3_file.runAndWait()
 
 
+# def save_voice_file(text, page):
+#     """Saves voice file to .mp3"""
+#     pyttsx3_file = pyttsx3.init()
+
+#     pyttsx3_file.save_to_file(text, f"{FILE_NAME}_{page}.mp3")
+#     pyttsx3_file.runAndWait()
+
+
 def pdf_to_speech(file=FILE_NAME, page=PAGE_NO):
     text = get_text_from_pdf(FILE_NAME, page)
-    speak_pdf(text)
+    txt_to_speech(text)
 
 
 def book_pdf_to_audiobook(file=FILE_NAME, start_page=1):
     """Reads outloud selected page from book."""
     with open(SAVE_DIRECTORY + file + ".json", "r") as json_file:
         pages = json.load(json_file)
-        page = pages[str(start_page)]
-        page_parsed = re.sub("\n", "", page)
-        print(f"Reading Page {start_page} of {len(pages)}:\n\n{page_parsed}\n")
-        speak_pdf(page_parsed)
+
+        cont = True
+        while cont:
+            page = pages[str(start_page)]
+            page_parsed = re.sub("\n", "", page)
+
+            txt_to_speech(f"Now Reading Page {start_page} of {len(pages)}:{page_parsed}")
+            print(f"Reading Page {start_page} of {len(pages)}:\n\n{page_parsed}\n")
+            txt_to_speech(page_parsed)
+            
+            txt_to_speech("Would you like to continue? Type y for yes or n for no.")
+            if input("Continue? y/n ") == "n":
+                cont = False
+            start_page += 1
+        
+
+
+
 
 
 # pdf_to_speech()
-save_pdf_book_to_json(FILE_NAME)
-# book_pdf_to_audiobook(start_page=23)
+# save_pdf_book_to_json(FILE_NAME)
+book_pdf_to_audiobook(start_page=23)
